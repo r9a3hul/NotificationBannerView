1888c6e6f698efbc641057c1d0dd906452f633f5
Rahul Chhetri <rahul.chhetri@sap.com>
2020-01-31 14:36:28 +0530

    Initial Commit

---
+++ NotificationBannerView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationBannerView
+//
+//  Created by Chhetri, Rahul on 31/01/20.
+//  Copyright © 2020 Chhetri, Rahul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
